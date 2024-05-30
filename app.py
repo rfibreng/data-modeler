e9b8aa9e1104f7b2e5f7107f0aed91896f07eee7
rubythalib33 <rubyabdullah14@gmail.com>
2024-05-30 12:40:11 +0700

    update

--- app.py
+++ app.py
@@ -33,11 +33,10 @@ warnings.simplefilter(action='ignore')
 # Page configuration
 st.set_page_config(page_title="Eyre", layout="wide", page_icon='assets/rect1.png')
 
-
 # Loading CSS
 utl.local_css("assets/custom.css")
 
-# Hiding hamburger menu from streamliti
+# Hiding hamburger menu from streamlit
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
@@ -47,13 +46,11 @@ hide_streamlit_style = """
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-
 @st.cache_data()
 def get_base64_of_bin_file(bin_file):
     with open(bin_file, 'rb') as f:
         data = f.read()
     return base64.b64encode(data).decode()
-
 
 def set_png_as_page_bg(png_file):
     bin_str = get_base64_of_bin_file(png_file)
@@ -69,19 +66,15 @@ def set_png_as_page_bg(png_file):
     st.markdown(page_bg_img, unsafe_allow_html=True)
     return
 
-
 set_png_as_page_bg('assets/bg.png')
 
-
 # Logo in side bar configuration
-st.sidebar.image("assets/eyre.png",
-                 output_format='PNG', width=200)
+st.sidebar.image("assets/eyre.png", output_format='PNG', width=200)
 
 # Sidebar Menu
 with st.sidebar:
-    menu_selected = option_menu("", ["Home", "Data Exploration", "Feature Engineering", "Modelling", "Experiment Logs", "Inference"],
-                                icons=["house", "card-list",
-                                       "columns-gap", "gear", "folder", "play", "folder"],
+    menu_selected = option_menu("", ["Home", "Data Exploration", "Feature Engineering", "Modelling", "Experiment Logs", "Inference", "Help Desk"],
+                                icons=["house", "card-list", "columns-gap", "gear", "folder", "play", "question-circle"],
                                 menu_icon="cast",
                                 default_index=0,
                                 styles={
@@ -91,38 +84,19 @@ with st.sidebar:
                                     "nav-link-selected": {"color": "#FF7F00", "background-color": "rgba(128, 128, 128, 0.1)"}
     })
 
-# Add Help Desk button in the header
-st.markdown(
-    """
-    <div style="display: flex; justify-content: space-between; align-items: center;">
-        <h2></h2>
-        <a href="https://dev05.overtech.id/" target="_blank">
-            <button style="background-color: #FF7F00; color: white; border: none; padding: 10px 20px; cursor: pointer;">Help Desk</button>
-        </a>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
-
 # Configuring home menu
 if menu_selected == "Home":
-    # st.write("Welcome")
     try:
         engine = create_engine(f"starrocks://{config['db_user']}:{config['db_password']}@{config['db_host']}:{config['db_port']}")
         connection = engine.connect()
         connection.execute(f"CREATE DATABASE IF NOT EXISTS {config['db_name']};")
     except:
         st.warning("Database is not connected please check is your database is on, or reload the page")
-    st.image("assets/header.png",
-             output_format='PNG')
+    st.image("assets/header.png", output_format='PNG')
 
 # Configuring data exploration menu
 if menu_selected == "Data Exploration":
     data_exploration_page(st)
-
-# # Configuring Data Editing Menu
-# if menu_selected == "Data Editing":
-#     data_editing_page(st)
 
 # Configuring Feature Engineering Menu
 if menu_selected == "Feature Engineering":
@@ -131,11 +105,17 @@ if menu_selected == "Feature Engineering":
 # Configuring Modelling Menu
 if menu_selected == "Modelling":
     modeling_page(st)
-    
 
+# Configuring Experiment Logs Menu
 if menu_selected == "Experiment Logs":
     experiment_log_page(st)
 
-# Configuring inference menu
+# Configuring Inference Menu
 if menu_selected == "Inference":
     inference_page(st)
+
+# Configuring Help Desk
+if menu_selected == "Help Desk":
+    js = "window.open('https://dev05.overtech.id/')"
+    html = '<img src onerror="{}">'.format(js)
+    st.markdown(html, unsafe_allow_html=True)
